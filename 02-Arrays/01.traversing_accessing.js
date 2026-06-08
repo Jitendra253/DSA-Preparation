dd4a63d1dadb1f76e0d81b208886b557f4638c31
Jitendra Kumar Mahali <107558211+Jitendra253@users.noreply.github.com>
2026-06-08 12:26:57 +0000

    updates

---
+++ 02-Arrays/01.traversing_accessing.js
@@ -0,0 +1,8 @@
+let data =[1,3,5,6,7,8,4,2,44,6,5,28];
+let eml=process.argv[2]
+
+for(let i = 0;i<data.length;i++){
+    console.log(`array index of (${i}) is ${data[i]}`);
+    
+}
+console.log(data[eml])
