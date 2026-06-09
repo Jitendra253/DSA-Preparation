e301f00b81f551bdc2675600e0e4774171cd3eb1
Jitendra Kumar Mahali <107558211+Jitendra253@users.noreply.github.com>
2026-06-09 00:47:57 +0000

    update delete

--- 02-Arrays/03.deleteElement.js
+++ 02-Arrays/03.deleteElement.js
@@ -1,10 +1,7 @@
 let arr = [1,2,3,4,5,6,7,8,9];
 let position = 4;
-for(i=position;i<arr.length;i++){
+for(i=position;i<arr.length-1;i++){
     arr[i] = arr[i+1]
 }
 arr.length = arr.length-1;
-console.log(arr)
-
-arr.splice(1,0,10);
 console.log(arr)
